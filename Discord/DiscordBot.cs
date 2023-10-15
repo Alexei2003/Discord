0bfa1582125c3084a8ea450efa3788813c22bf15
Aliaksei Dubouski <91179353+Alexei2003@users.noreply.github.com>
2023-10-15 14:05:26 +0300

    Initial commit

---
+++ Discord/DiscordBot.cs
@@ -0,0 +1,51 @@
+﻿using Discord.Commands;
+using Discord.WebSocket;
+using Newtonsoft.Json;
+using System.Windows.Input;
+
+namespace Discord
+{
+    internal class DiscordBot
+    {
+        private readonly DiscordSocketClient discord = new();
+        private readonly CommandService command = new();
+
+        private readonly Random rand = new Random();
+
+        private readonly List<string> messagePrinzEugen;
+
+        public DiscordBot()
+        {
+            var accessToken = File.ReadAllText("AccessToken.txt");
+
+            var str = File.ReadAllText("PrinzEugen.txt");
+            messagePrinzEugen = JsonConvert.DeserializeObject<List<string>>(str);
+
+            discord.LoginAsync(TokenType.Bot, accessToken);
+            discord.StartAsync();
+
+            discord.MessageReceived += HandleCommandAsync;
+
+            Task.Delay(-1);
+        }
+
+        private async Task HandleCommandAsync(SocketMessage messageParam)
+        {
+
+            var message = messageParam as SocketUserMessage;
+            if (message == null || message.Author.IsBot)
+            {
+                return;
+            }
+
+            var context = new SocketCommandContext(discord, message);
+
+            command.ExecuteAsync(context, 0, null);
+
+            if (rand.Next(5) == 0)
+            {
+                context.Channel.SendMessageAsync(messagePrinzEugen[rand.Next(rand.Next(messagePrinzEugen.Count))]);
+            }
+        }
+    }
+}
---
+++ Discord/Program.cs
@@ -0,0 +1,32 @@
+﻿
+using Discord;
+using Newtonsoft.Json;
+
+namespace HelloWorld
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            var discord = new DiscordBot();
+
+            while (true)
+            {
+                var a = discord.Equals(null);
+            }
+
+            /*            var str  = Console.ReadLine();
+
+                        List<string> list = new List<string>();
+
+                        while(str != "F")
+                        {
+                            list.Add(str);
+                            str = Console.ReadLine();
+                        }
+
+                        Console.WriteLine(JsonConvert.SerializeObject(list));*/
+        }
+    }
+}
